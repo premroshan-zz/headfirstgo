ed4eea8671f10344e4246eca765ea0955e837fc2
Prem Roshan Madhusudhan Nair <prem.roshan.madhusudhan.nair@sap.com>
2021-05-29 07:50:24 +0530

    v

--- chapter5/average/main.go
+++ chapter5/average/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/premroshan/headfirstgo/chapter5/datafile"
+)
 
 func main() {
-	numbers := [3]float64{21.54, 21.8, 13.4}
+	//numbers := [3]float64{21.54, 21.8, 13.4}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, value := range numbers {
 		sum += value
